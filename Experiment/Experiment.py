89716f3a3344921e4928862d60e45a3e86fae454
Amy Reed <amy_hindman@yahoo.com>
2014-10-18 15:19:15 -0500

    fixing pylint feedback

--- Experiment/Experiment.py
+++ Experiment/Experiment.py
@@ -1,11 +1,9 @@
-import sys
-
-def largest(list):
-  if len(list) == 0:
-    raise ValueError("Cannot call largest on empty list")
+def largest(mylist):
+    if len(mylist) == 0:
+      raise ValueError("Cannot call largest on empty list")
   # max = -sys.maxint # "smallest" possible int
-  max = list[0]
-  for index in range(len(list)):
-    if (list[index] > max):
-      max = list[index]
-  return max
+  thismax = mylist[0]
+  for index in range(len(mylist)):
+      if mylist[index] > thismax:
+        thismax = mylist[index]
+  return thismax
