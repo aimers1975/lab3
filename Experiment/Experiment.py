1656ee5c6c0e797449a68038dd6ed7335007659b
Amy Reed <amy_hindman@yahoo.com>
2014-10-18 15:28:41 -0500

    fixing indent

--- Experiment/Experiment.py
+++ Experiment/Experiment.py
@@ -1,6 +1,7 @@
 """This is the largest function"""
+
 def largest(mylist):
-"""This checks for largest in a list, returns ValueError on empty list"""
+    """This checks for largest in a list, returns ValueError on empty list"""
     if len(mylist) == 0:
         raise ValueError("Cannot call largest on empty list")
     # max = -sys.maxint # "smallest" possible int
