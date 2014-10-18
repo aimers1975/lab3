1d5785398eed2cd2c9c66045f34194737af48d22
Amy Reed <amy_hindman@yahoo.com>
2014-10-18 12:18:34 -0500

    rming unnecessary files

--- Test1
+++
@@ -1,8 +0,0 @@
-public class myTest {
-
-  int myvar = 0;
-  
-  public myTest(int newvar) {
-    myvar = newvar;
-  }
-}
--- myhashtest.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Collections;
-
-public class myhashtest {
-	
-  HashMap<String,Integer> ahash = new HashMap<String,Integer>();	
-	
-}
