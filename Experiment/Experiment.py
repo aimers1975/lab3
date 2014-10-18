46d7a5ebb7361056e6849125379ba8a9a472d10d
Amy Reed <amy_hindman@yahoo.com>
2014-10-18 15:22:08 -0500

    fixing indentations

--- Experiment/Experiment.py
+++ Experiment/Experiment.py
@@ -1,9 +1,9 @@
 def largest(mylist):
     if len(mylist) == 0:
-      raise ValueError("Cannot call largest on empty list")
+        raise ValueError("Cannot call largest on empty list")
   # max = -sys.maxint # "smallest" possible int
-  thismax = mylist[0]
-  for index in range(len(mylist)):
-      if mylist[index] > thismax:
-        thismax = mylist[index]
-  return thismax
+    thismax = mylist[0]
+    for index in range(len(mylist)):
+        if mylist[index] > thismax:
+      	    thismax = mylist[index]
+    return thismax
