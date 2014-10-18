54dc929f64c825987ac37e7efa975a5eb4d2d655
Amy Reed <amy_hindman@yahoo.com>
2014-10-18 15:26:13 -0500

    tried adding docstrings

--- Experiment/Experiment.py
+++ Experiment/Experiment.py
@@ -1,9 +1,11 @@
+"""This is the largest function"""
 def largest(mylist):
+"""This checks for largest in a list, returns ValueError on empty list"""
     if len(mylist) == 0:
         raise ValueError("Cannot call largest on empty list")
-  # max = -sys.maxint # "smallest" possible int
+    # max = -sys.maxint # "smallest" possible int
     thismax = mylist[0]
     for index in range(len(mylist)):
         if mylist[index] > thismax:
-      	    thismax = mylist[index]
+            thismax = mylist[index]
     return thismax
