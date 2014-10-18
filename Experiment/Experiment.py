e78d6c80f2b953aab9369c5a8dd376782c0189b2
Amy Reed <amy_hindman@yahoo.com>
2014-10-18 16:18:05 -0500

    fixed test

--- Experiment/Experiment.py
+++ Experiment/Experiment.py
@@ -5,7 +5,7 @@ def largest(mylist):
     if len(mylist) == 0:
         raise ValueError("Cannot call largest on empty list")
     # max = -sys.maxint # "smallest" possible int
-    thismax = 0
+    thismax = mylist[0]
     for index in range(len(mylist)):
         if mylist[index] > thismax:
             thismax = mylist[index]
